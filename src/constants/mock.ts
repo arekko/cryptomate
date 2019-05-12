d3c6101c09481b3e986f5f6bd45e20cfee464dde
Andrei Vasilev <andreidev@arekko.home>
2019-05-12 15:37:54 +0300

    add mock data

---
+++ src/constants/mock.ts
@@ -0,0 +1,55 @@
+export const btcNews = [
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url:
+      "https://www.newsbtc.com/wp-content/uploads/2019/05/shutterstock_1025239828-700x400.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url: "https://icobrothers.media/wp-content/uploads/2019/03/15-1024x538.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url:
+      "https://www.newsbtc.com/wp-content/uploads/2019/05/shutterstock_1025239828-700x400.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url:
+      "https://www.newsbtc.com/wp-content/uploads/2019/05/shutterstock_1025239828-700x400.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url: "https://icobrothers.media/wp-content/uploads/2019/03/15-1024x538.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url:
+      "https://www.newsbtc.com/wp-content/uploads/2019/05/shutterstock_1025239828-700x400.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url: "https://icobrothers.media/wp-content/uploads/2019/03/15-1024x538.jpg"
+  },
+  {
+    title: "Circle Adds 'Buy the Market Feature to Crypto Investment App",
+    text:
+      "After a soft launch back in March 2018, the company is making the app with its full suite of services available as of Tuesday. Circle first announced its investor-focused app back in November and later released it in 46 U.S. states as part of an early access availability phase.",
+    url:
+      "https://www.newsbtc.com/wp-content/uploads/2019/05/shutterstock_1025239828-700x400.jpg"
+  }
+];
