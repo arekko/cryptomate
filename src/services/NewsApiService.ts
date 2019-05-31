b181bf1346b275524759d82e323e4e5f9b1825ee
Andrei Vasilev <amlk158@gmail.com>
2019-05-31 12:26:56 +0300

    Update NewsApiService.ts

--- src/services/NewsApiService.ts
+++ src/services/NewsApiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class NewsApiService {
-  private API_KEY = "2cda9e9005464b6987b4799fe7311336";
+  private API_KEY = "";
   private URL = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${
     this.API_KEY
   }`;
